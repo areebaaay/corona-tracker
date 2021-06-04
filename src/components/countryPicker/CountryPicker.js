33aad1288fa95c49686ed9ea6f723a74ad43a352
Areeba Akhtar <areeba.akhtar776@gmail.com>
2021-06-04 12:18:48 +0500

    select option added

--- src/components/countryPicker/CountryPicker.js
+++ src/components/countryPicker/CountryPicker.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
 
-const CountryPicker = () => {
-  return <h1>CountryPicker</h1>;
+const CountryPicker = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+    fetchApi();
+  }, [setFetchedCountries]);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        default=""
+        onChange={(e) => {
+          handleCountryChange(e.target.value);
+        }}>
+        <option value="">Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default CountryPicker;
